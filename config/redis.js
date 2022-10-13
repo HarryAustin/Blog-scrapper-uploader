10f190938a180a8179a51eb04009c623199e568e
HarryAustin <osagiedeharrison@yahoo.com>
2022-10-14 00:48:36 +0100

    changed redis url

--- config/redis.js
+++ config/redis.js
@@ -1,14 +1,8 @@
 require("dotenv").config();
 const Redis = require("ioredis");
 
-const options = {
-  port: process.env.REDIS_PORT || 6379, // Redis port
-  host: process.env.REDIS_HOST || "127.0.0.1", // Redis host
-  // username: "default", // needs Redis >= 6
-  // password: "my-top-secret",
-  // db: 0, // Defaults to 0
-};
+const REDIS_URL = process.env.REDIS_URL || "localhost:6379";
 
-const redis = new Redis(options);
+const redis = new Redis(REDIS_URL);
 
 module.exports = redis;
