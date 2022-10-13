c31730c33fb92e957c62541db93e7ac930336a86
HarryAustin <osagiedeharrison@yahoo.com>
2022-10-14 00:27:50 +0100

    redis config

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,14 @@
+require("dotenv").config();
 const Redis = require("ioredis");
 
-const redis = new Redis();
+const options = {
+  port: process.env.REDIS_PORT || 6379, // Redis port
+  host: process.env.REDIS_HOST || "127.0.0.1", // Redis host
+  // username: "default", // needs Redis >= 6
+  // password: "my-top-secret",
+  // db: 0, // Defaults to 0
+};
+
+const redis = new Redis(options);
 
 module.exports = redis;
