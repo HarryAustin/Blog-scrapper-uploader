1ccd6291bf968a7697116b8b9667a4ce2a0ed318
HarryAustin <osagiedeharrison@yahoo.com>
2022-10-14 20:01:13 +0100

    link font files to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
+import "./fonts/FreightSansProBold-Regular.ttf";
+import "./fonts/FreightSansProLight-Regular.ttf";
+import "./fonts/FreightSansProMedium-Regular.ttf";
+import "./fonts/FreightSansProSemibold-Regular.ttf";
 import App from "./App";
 
 ReactDOM.render(
